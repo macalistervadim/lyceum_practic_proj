fa8f896b7bc22602c5c10136fb4f505a1b565452
macalistervadim <macalistervadim@yandex.ru>
2024-02-05 19:28:04 +0600

    Homework porcess..

--- lyceum/lyceum/settings.py
+++ lyceum/lyceum/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'about.apps.AboutConfig'
+    'catalog.apps.CatalogConfig'
+    'homepage.apps.HomepageConfig'
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
