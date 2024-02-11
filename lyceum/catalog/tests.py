fc5ca5e104e89e733e7d88fc5fbc994c2f6a758e
macalistervadim <macalistervadim@yandex.ru>
2024-02-11 20:34:12 +0600

    Change CD/CD

--- lyceum/catalog/tests.py
+++ lyceum/catalog/tests.py
@@ -1,7 +1,11 @@
 from django.test import Client, TestCase
 
 
-class CatalogPageTest(TestCase):
-    def test_catalog_page(self):
-        response = Client().get("/catalog/")
-        self.assertEqual(response.status_code, 200)
+class UrlTests(TestCase):
+    def test_description(self):
+        response = Client().get("/catalog", follow=True)
+        self.assertEqual(response.status_code, 200, "Статус код не 200")
+
+    def test_item_detail(self):
+        response = Client().get("/catalog/1", follow=True)
+        self.assertEqual(response.status_code, 200, "Статус код не 200")
