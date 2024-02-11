a7d06af56bf84e226042f01711107dc417381c4c
macalistervadim <macalistervadim@yandex.ru>
2024-02-11 20:01:16 +0600

    Change CD/CD

--- lyceum/lyceum/settings.py
+++ lyceum/lyceum/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY", default="key").lower()
 
 DEBUG = env("DJANGO_DEBUG", default="True")
 
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["127.0.0.1"])
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["* 127.0.0.1"])
 
 
 INSTALLED_APPS = [
@@ -43,6 +43,7 @@ MIDDLEWARE = [
 
 INTERNAL_IPS = [
     "127.0.0.1",
+    "localhost"
 ]
 
 ROOT_URLCONF = "lyceum.urls"
