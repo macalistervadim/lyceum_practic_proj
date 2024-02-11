6cd89e8ceb7ece9ffca154d8e96d2d2d861b8694
macalistervadim <macalistervadim@yandex.ru>
2024-02-11 19:46:07 +0600

    Change CD/CD

--- lyceum/lyceum/settings.py
+++ lyceum/lyceum/settings.py
@@ -10,11 +10,11 @@ env = environ.Env(DEBUG=(bool, False))
 
 env.read_env(os.path.join(BASE_DIR, ".env"))
 
-SECRET_KEY = env("DJANGO_SECRET_KEY", default="key")
+SECRET_KEY = env("DJANGO_SECRET_KEY", default="key").lower()
 
 DEBUG = env("DJANGO_DEBUG", default="True")
 
-ALLOWED_HOSTS = env.str("DJANGO_ALLOWED_HOSTS", default="127.0.0.1").split(",")
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["127.0.0.1"])
 
 
 INSTALLED_APPS = [
