9c1114c0d8d8307fb106253d7a677365290af3d0
macalistervadim <macalistervadim@yandex.ru>
2024-02-15 17:20:37 +0600

    formattig code

--- lyceum/homepage/apps.py
+++ lyceum/homepage/apps.py
@@ -5,3 +5,4 @@ from django.apps import AppConfig
 class HomepageConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "homepage"
+    verbose_name = "Домашняя страница"
