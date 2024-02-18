d3195e5ce515dcfb0e1f77c3663c9aee7415eaf3
macalistervadim <macalistervadim@yandex.ru>
2024-02-18 20:02:18 +0600

    formatting code

--- lyceum/lyceum/settings.py
+++ lyceum/lyceum/settings.py
@@ -111,4 +111,3 @@ if DEBUG:
     INSTALLED_APPS.append("debug_toolbar")
     MIDDLEWARE.insert(0, "debug_toolbar.middleware.DebugToolbarMiddleware")
     INTERNAL_IPS = ["127.0.0.1", "localhost"]
-
