40857cb79d805da9cfac921f84b25a18422daedf
macalistervadim <macalistervadim@yandex.ru>
2024-02-18 20:01:55 +0600

    formatting code

--- lyceum/lyceum/urls.py
+++ lyceum/lyceum/urls.py
@@ -15,4 +15,5 @@ if settings.DEBUG:
     import debug_toolbar
 
     urlpatterns += (path("__debug__/", include(debug_toolbar.urls)),)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
