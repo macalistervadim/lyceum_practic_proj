b6f36018274d1657c4374c66b54f4317ea80b2f2
macalistervadim <macalistervadim@yandex.ru>
2024-03-03 22:02:05 +0600

    Change catalog/test_context

--- lyceum/catalog/views.py
+++ lyceum/catalog/views.py
@@ -3,13 +3,14 @@ import django.shortcuts
 
 import catalog.models
 
-
 def item_list(request):
     template = "catalog/item_list.html"
     items = (
         catalog.models.Item.objects.filter(is_published=True)
         .order_by("category__name", "name")
         .only(
+            "id",
+            "category_id",
             "name",
             "text",
         )
@@ -24,10 +25,11 @@ def item_list(request):
     return django.shortcuts.render(request, template, context)
 
 
+
 def item_detail(request, pk):
     template = "catalog/item.html"
     queryset = (
-        catalog.models.Item.objects.select_related("category", "mainimage")
+        catalog.models.Item.objects.select_related("category")
         .prefetch_related(
             django.db.models.Prefetch(
                 "tags", queryset=catalog.models.Tag.objects.only("name"),
@@ -37,10 +39,11 @@ def item_detail(request, pk):
         .only("name", "text", "category__name")
     )
     item = django.shortcuts.get_object_or_404(queryset, pk=pk)
-    main_image = item.mainimage
+    
+    main_image = item.mainimage if hasattr(item, 'mainimage') else None
+    main_image_url = get_thumbnail_url(main_image) if main_image else None
+    
     gallery_images = item.gallery_images.all()
-
-    main_image_url = get_thumbnail_url(main_image)
     gallery_images_urls = [
         get_thumbnail_url(image) for image in gallery_images
     ]
@@ -51,7 +54,6 @@ def item_detail(request, pk):
         "gallery_images_urls": gallery_images_urls,
     }
     return django.shortcuts.render(request, template, context)
-
 
 def get_thumbnail_url(image):
     if image:
