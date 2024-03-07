d8cc36f88207bcfe3db6cd552c0042383389c427
macalistervadim <macalistervadim@yandex.ru>
2024-03-07 22:07:37 +0600

    Removed more folder from version control

--- lyceum/static/js/current_date.js
+++
@@ -1,8 +0,0 @@
-const copyrightElement = document.getElementById("current-year");
-
-const currDate = new Date();
-const dateDiff = Math.abs((serverDate - currDate) / (3600*1000));
-
-if (dateDiff < 24) {
-    copyrightElement.textContent = String(currDate.getFullYear());
-}
